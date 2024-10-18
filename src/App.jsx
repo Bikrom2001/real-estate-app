36077573a53e9857f43db3c80d71b3edce5b3354
Bikrom-Roy <bikromroy2001@gmail.com>
2024-10-18 21:56:33 +0600

    footer page create

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Footer from "./sections/Footer";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="overflow-hidden">
+      <Footer />
+    </main>
+  );
 }
 
 export default App;
---
+++ src/constants/index.jsx
@@ -0,0 +1,25 @@
+import {
+  FooterFacebokLogo,
+  FooterInstagramLogo,
+  FooterLinkedinLogo,
+  FooterTwitterLogo,
+} from "./ImageExport";
+
+export const Footerlogos = [
+  {
+    id: "0",
+    url: <FooterFacebokLogo />,
+  },
+  {
+    id: "1",
+    url: <FooterTwitterLogo />,
+  },
+  {
+    id: "2",
+    url: <FooterInstagramLogo />,
+  },
+  {
+    id: "3",
+    url: <FooterLinkedinLogo />,
+  },
+];
