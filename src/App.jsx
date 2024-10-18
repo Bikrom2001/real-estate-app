dac399a0ffcb258ceae8179f55a39d5dc03d6852
Bikrom-Roy <bikromroy2001@gmail.com>
2024-10-18 22:17:03 +0600

    create a contac us page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Contact from "./sections/Contact";
 import Footer from "./sections/Footer";
 
 function App() {
   return (
     <main className="overflow-hidden">
+      <Contact />
       <Footer />
     </main>
   );
